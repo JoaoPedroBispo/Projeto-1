acec6493758746c116e1cc20797653ce5c50877e
JoaoPedroBispo <jopbispo83@gmail.com>
2023-08-07 12:24:30 -0300

    Atualização

--- restaurante/src/pages/Cardapio/index.tsx
+++ restaurante/src/pages/Cardapio/index.tsx
@@ -1,7 +1,12 @@
+import style from './Cardapio.module.scss';
+import logo from '../../assets/logo.svg'
+
 export default function Cardapio(){
     return (
         <main>
-            <nav>Restaurante</nav>
+            <nav className={style.menu}>
+                <img src={logo} alt="logo" />
+            </nav>
         </main>
     );
 }
