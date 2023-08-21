6c33c964e3202218207ca25e42813f46da6290a0
JoaoPedroBispo <jopbispo83@gmail.com>
2023-08-21 12:21:12 -0300

    Adicionamento do header

--- restaurante/src/pages/Cardapio/index.tsx
+++ restaurante/src/pages/Cardapio/index.tsx
@@ -1,5 +1,5 @@
 import style from './Cardapio.module.scss';
-import logo from '../../assets/logo.svg'
+import logo from 'assets/logo.svg'
 
 export default function Cardapio(){
     return (
@@ -7,6 +7,11 @@ export default function Cardapio(){
             <nav className={style.menu}>
                 <img src={logo} alt="logo" />
             </nav>
+            <header className={style.header}>
+                <div className={style.header__text}>
+                    A casa do código e da massa !!!
+                </div>
+            </header>
         </main>
     );
 }
