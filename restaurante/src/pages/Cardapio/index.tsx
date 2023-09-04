a2bd94a76cfdea4cd16956f3d8a918054ef92ea3
JoaoPedroBispo <jopbispo83@gmail.com>
2023-09-04 12:18:37 -0300

    Atualização
    (Criação da barra de busca)

--- restaurante/src/pages/Cardapio/index.tsx
+++ restaurante/src/pages/Cardapio/index.tsx
@@ -1,17 +1,24 @@
-import style from './Cardapio.module.scss';
+import styles from './Cardapio.module.scss';
 import logo from 'assets/logo.svg'
+import Buscador from './Buscador';
+import { useState } from 'react';
 
 export default function Cardapio(){
+    const [busca, setBusca] = useState("");
     return (
         <main>
-            <nav className={style.menu}>
+            <nav className={styles.menu}>
                 <img src={logo} alt="logo" />
             </nav>
-            <header className={style.header}>
-                <div className={style.header__text}>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
                     A casa do código e da massa !!!
                 </div>
             </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+                <Buscador busca={busca} setBusca={setBusca}/>
+            </section>
         </main>
     );
 }
