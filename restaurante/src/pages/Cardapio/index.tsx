35cfcd1565fef2c8d821bb27dcae5d874c650785
JoaoPedroBispo <jopbispo83@gmail.com>
2023-10-05 11:46:26 -0300

    Atualização

--- restaurante/src/pages/Cardapio/index.tsx
+++ restaurante/src/pages/Cardapio/index.tsx
@@ -6,12 +6,13 @@ import Filtros from "./Filtros";
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null);
   return (
     <main>
       <nav className={styles.menu}>
         <img src={logo} alt="logo" />
       </nav>
-      
+
       <header className={styles.header}>
         <div className={styles.header__text}>
           A casa do código e da massa !!!
@@ -21,9 +22,9 @@ export default function Cardapio() {
       <section className={styles.cardapio}>
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
         <Buscador busca={busca} setBusca={setBusca} />
-        
+
         <div className={styles.cardapo__filtros}>
-          <Filtros />
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
         </div>
       </section>
     </main>
